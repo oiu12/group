3313e90d38a6bdbd869868e89e28e0b493df1313
oiu12 <oiu1224@163.com>
2016-10-17 10:37:11 +0800

    Update server.R

--- server.R
+++ server.R
@@ -1,11 +1,31 @@
-library(shiny)
+library(maps)
+library(mapproj)
+counties <- readRDS("data/counties.rds")
+source("helpers.R")
 
- 
-shinyServer(function(input, output) {
+shinyServer(
+  function(input, output) {
+    output$map <- renderPlot({
+      data <- switch(input$var, 
+        "Percent White" = counties$white,
+        "Percent Black" = counties$black,
+        "Percent Hispanic" = counties$hispanic,
+        "Percent Asian" = counties$asian)
 
-     output$text1 <- renderText({ 
-          paste("You have selected this",input$range,sep = " ",collapse = ", ")
-     })
-
+      color <- switch(input$var, 
+        "Percent White" = "darkgreen",
+        "Percent Black" = "black",
+        "Percent Hispanic" = "darkorange",
+        "Percent Asian" = "darkviolet")
+    
+      legend <- switch(input$var, 
+        "Percent White" = "% White",
+        "Percent Black" = "% Black",
+        "Percent Hispanic" = "% Hispanic",
+        "Percent Asian" = "% Asian")
+  
+  
+      percent_map(var = data, color = color, legend.title = legend,  max = input$range[2], min = input$range[1])
+    })
   }
 )
